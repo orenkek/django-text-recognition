0decda45501303bc0f68987d8e0e1a9680c4612b
anuarcheck <anuarcheck@gmail.com>
2022-05-22 13:42:24 +0500

    feat: Добавил шаблоны

--- base/views.py
+++ base/views.py
@@ -5,4 +5,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def base(request):
-    return HttpResponse('Hello, World!')
+    return render(request, 'base/base.html', {})
--- django_text_recognition/settings.py
+++ django_text_recognition/settings.py
@@ -57,7 +57,9 @@ ROOT_URLCONF = 'django_text_recognition.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
